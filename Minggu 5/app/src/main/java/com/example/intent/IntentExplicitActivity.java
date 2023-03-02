350d747fd9c640cefe47191475868ffb1c267c7c
Tryit-All <fathurrahman.dk@gmail.com>
2023-03-02 08:33:21 +0700

    Add Project Minggu 5

--- Minggu 5/app/src/main/java/com/example/intent/IntentExplicitActivity.java
+++ Minggu 5/app/src/main/java/com/example/intent/IntentExplicitActivity.java
@@ -2,13 +2,41 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class IntentExplicitActivity extends AppCompatActivity {
+    EditText kode;
+    Button add;
+    private String KEY_KODE = "KODE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_explicit);
+
+        kode = (EditText) findViewById(R.id.kode);
+        add = (Button) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String kode_e = kode.getText().toString();
+                    if (kode_e.equals("e41210628")){
+                        Intent i = new Intent(IntentExplicitActivity.this, ExplicitL2Activity.class);
+                        i.putExtra(KEY_KODE, kode_e);
+                        startActivity(i);
+                    }else {
+                        Toast.makeText(IntentExplicitActivity.this, "YOU NEED TO FILL YOUR NAME", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
--- Minggu 5/app/src/main/java/com/example/intent/IntentImplicitActivity.java
+++ Minggu 5/app/src/main/java/com/example/intent/IntentImplicitActivity.java
@@ -2,13 +2,32 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class IntentImplicitActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_implicit);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.textEdit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }
