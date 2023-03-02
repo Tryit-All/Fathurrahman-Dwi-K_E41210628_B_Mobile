1cf4d14ccbff29d6abb1f9de76c20f862ad41db5
Tryit-All <fathurrahman.dk@gmail.com>
2023-03-02 08:33:03 +0700

    Add Project Minggu 4

--- Minggu 4/app/src/main/java/com/example/fragment/FragmentFirstActivity.java
+++ Minggu 4/app/src/main/java/com/example/fragment/FragmentFirstActivity.java
@@ -1,14 +1,32 @@
 package com.example.fragment;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class FragmentFirstActivity extends AppCompatActivity {
-
+public class FragmentFirstActivity extends Fragment {
+    View view;
+    Button button;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment_first);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.activity_fragment_first, container, false);
+
+        button = view.findViewById(R.id.Button_first);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "ini Fragmen Pertama", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
     }
 }
--- Minggu 4/app/src/main/java/com/example/fragment/SecondFragmentActivity.java
+++ Minggu 4/app/src/main/java/com/example/fragment/SecondFragmentActivity.java
@@ -1,14 +1,32 @@
 package com.example.fragment;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class SecondFragmentActivity extends AppCompatActivity {
-
+public class SecondFragmentActivity extends Fragment {
+    View view;
+    Button button;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second_fragment);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.activity_second_fragment, container, false);
+
+        button = view.findViewById(R.id.Button_second);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "ini Fragmen Pertama", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
     }
 }
