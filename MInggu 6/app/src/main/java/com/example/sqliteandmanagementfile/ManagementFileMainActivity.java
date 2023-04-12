867ef5541adaaacb7c8118927e065e24b344c222
Tryit-All <fathurrahman.dk@gmail.com>
2023-04-12 11:58:40 +0700

    Update Project Minggu 6

--- MInggu 6/app/src/main/java/com/example/sqliteandmanagementfile/ManagementFileMainActivity.java
+++ MInggu 6/app/src/main/java/com/example/sqliteandmanagementfile/ManagementFileMainActivity.java
@@ -1,14 +1,72 @@
 package com.example.sqliteandmanagementfile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.lang.reflect.Field;
 
 public class ManagementFileMainActivity extends AppCompatActivity {
+    private  int STORAGE_PERMISSIO_CODE = 28;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.management_file_main);
+        editText = findViewById(R.id.textField);
+    }
+
+    public void next(View view){
+        Intent intent = new Intent(ManagementFileMainActivity.this, ManagementFileValueActivity.class);
+        startActivity(intent);
+    }
+
+    public void savePublic(View view){
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSIO_CODE);
+        String info = editText.getText().toString();
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File fileName = new File(folder, "HasilData1.txt");
+        writeData(fileName, info);
+        editText.setText("");
+    }
+
+    public void savePrivate(View view){
+        String info = editText.getText().toString();
+        File folder = getExternalFilesDir("fathur");
+        File fileName = new File(folder, "HasilData2.txt");
+        writeData(fileName, info);
+        editText.setText("");
+    }
+
+    private void writeData(File fileName, String info) {
+        FileOutputStream fileOutputStream = null;
+
+        try {
+            System.out.println(info.getBytes());
+            fileOutputStream = new FileOutputStream(fileName);
+                fileOutputStream.write(info.getBytes());
+            Toast.makeText(this, "Done" + fileName.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fileOutputStream != null){
+                try {
+                    fileOutputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- MInggu 6/app/src/main/java/com/example/sqliteandmanagementfile/SqliteAddDataActivity.java
+++ MInggu 6/app/src/main/java/com/example/sqliteandmanagementfile/SqliteAddDataActivity.java
@@ -2,13 +2,77 @@ package com.example.sqliteandmanagementfile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class SqliteAddDataActivity extends AppCompatActivity {
+    protected Cursor cursor;
+    DatePickerDialog pickerDialog;
+    DataHelper dbHelper;
+    Button b1, b2;
+    EditText t1, t2, t3, t4, t5;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sqlite_add_data);
+
+        dbHelper = new DataHelper(this);
+        t1 = (EditText) findViewById(R.id.t1);
+        t2 = (EditText) findViewById(R.id.t2);
+        t3 = (EditText) findViewById(R.id.t3);
+        t4 = (EditText) findViewById(R.id.t4);
+        t5 = (EditText) findViewById(R.id.t5);
+        b1 = (Button) findViewById(R.id.save);
+        b2 = (Button) findViewById(R.id.back);
+
+        t3.setInputType(InputType.TYPE_NULL);
+        t3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                pickerDialog = new DatePickerDialog(SqliteAddDataActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                t3.setText(dayOfMonth + "/" + month + "/" + year);
+                            }
+                        }, year, month, day);
+                pickerDialog.show();
+            }
+        });
+
+        b1.setOnClickListener((arg0) -> {
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            db.execSQL("insert into biodata(no, nama, tgl, jk, alamat) values ('"+
+                    t1.getText().toString() +"','"+
+                    t2.getText().toString() +"','"+
+                    t3.getText().toString() +"','"+
+                    t4.getText().toString() +"','"+
+                    t5.getText().toString() +"')");
+            Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_LONG).show();
+            SqliteMainActivity.ma.RefreshList();
+            finish();
+        });
+        b2.setOnClickListener((arg0) -> {
+            finish();
+        });
+
     }
 }
